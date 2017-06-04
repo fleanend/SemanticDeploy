141aad4f1ae097165fbcfd36280923adc389e115
fleanend <edoardoferrante@hotmail.it>
2017-06-04 19:20:54 +0200

    Add TTS project

---
+++ TTs/Progetto2/MyForm.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <windows.h>
+#include <mmsystem.h>
+#include <msclr/marshal_cppstd.h>
+
+namespace Progetto2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace TTS;
+
+	/// <summary>
+	/// Riepilogo per MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: aggiungere qui il codice del costruttore.
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Pulire le risorse in uso.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Variabile di progettazione necessaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+		/// il contenuto del metodo con l'editor di codice.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(90, 46);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->Text = L"What\'s your name\?";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(119, 163);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 13);
+			this->label1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(101, 96);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Done";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		System::String^ wavFile = TextToSpeechHandler::TextToSpeech(this->textBox1->Text);
+
+		std::string wavFileN = msclr::interop::marshal_as< std::string >(wavFile);
+		
+		std::wstring stemp = std::wstring(wavFileN.begin(), wavFileN.end());
+		LPCWSTR sysWavFile = stemp.c_str();
+
+		this->label1->Text = wavFile;
+
+		PlaySound(sysWavFile, NULL, SND_FILENAME);
+
+		DeleteFile(sysWavFile);
+
+	}
+
+	}
+	; }
---
+++ TTs/TTs/TextToSpeechHandler.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Speech.Synthesis;
+
+namespace TTS
+{
+
+    public class TextToSpeechHandler
+    {
+
+        public static String TextToSpeech(String text, int voiceIndex = 0, bool voiceIntroduce = false)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            synth.SetOutputToWaveFile("msg.wav");
+
+            
+
+            ArrayList voices = new ArrayList();
+
+            foreach (InstalledVoice voice in synth.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+
+                voices.Add(info.Name);
+            }
+
+            if (voiceIndex > synth.GetInstalledVoices().Count)
+            {
+                voiceIndex = synth.GetInstalledVoices().Count;
+            }
+
+            synth.SelectVoice(voices[voiceIndex].ToString());
+
+            String msg = text;
+
+            if (voiceIntroduce)
+            {
+                msg = "Hi, my name is " + voices[voiceIndex];
+            }
+
+            synth.Speak(msg);
+            synth.Dispose();
+
+            return Path.GetFullPath("msg.wav");
+        }
+
+        public static String TextToSpeech(String text, int voiceIndex = 0)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            synth.SetOutputToWaveFile("msg.wav");
+
+
+
+            ArrayList voices = new ArrayList();
+
+            foreach (InstalledVoice voice in synth.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+
+                voices.Add(info.Name);
+            }
+
+            if (voiceIndex > synth.GetInstalledVoices().Count)
+            {
+                voiceIndex = synth.GetInstalledVoices().Count;
+            }
+
+            synth.SelectVoice(voices[voiceIndex].ToString());
+
+            String msg = text;
+
+            synth.Speak(msg);
+            synth.Dispose();
+
+            return Path.GetFullPath("msg.wav");
+        }
+
+        public static String TextToSpeech(String text)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            synth.SetOutputToWaveFile("msg.wav");
+
+
+
+            ArrayList voices = new ArrayList();
+
+            foreach (InstalledVoice voice in synth.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+
+                voices.Add(info.Name);
+            }
+
+
+            synth.SelectVoice(voices[0].ToString());
+
+            String msg = text;
+
+            synth.Speak(msg);
+            synth.Dispose();
+
+            return Path.GetFullPath("msg.wav");
+        }
+    }
+}
